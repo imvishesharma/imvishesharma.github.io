e28b22bcc621cfa4f8ce30b65e1730d13a88315d
Vishesh Sharma <imvishesharma@gmail.com>
2021-08-27 12:09:28 +0530

    Rebuilt
    Removed W3.css

--- my.js
+++ my.js
@@ -1,7 +1,4 @@
-document.getElementById("opq1").addEventListener("click", hideQuote);
-
-function hideQuote() {
-    document.getElementById("opq1").style.display = "none";
-    document.getElementById("divx").style.opacity = "100%";
-    document.getElementById("div1").style.backgroundColor = "white";
-}
+console.log(screen.width);
+console.log(window.innerWidth);
+document.getElementById("img").style.width = screen.width + "px";
+document.getElementById("img").style.height = screen.height + "px";
